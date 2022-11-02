83535fc0c56dfea632c1a44a59fd238881633800
seung yeon <zionimobu@gmail.com>
2022-11-02 17:46:09 +0900

    1102 수정

---
+++ redux-toolkit/src/components/Count.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { increment, incrementByAmount } from "../modules/counter";
+
+const Count = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => {
+          // incrementByAmount()의 인수값으로 들어가는 값는 payload로 전달됨
+          dispatch(incrementByAmount(10));
+        }}
+      >
+        +10
+      </button>
+    </div>
+  );
+};
+
+export default Count;
--- redux-toolkit/src/modules/counter.js
+++ redux-toolkit/src/modules/counter.js
@@ -1,18 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// createSlice에 초기값과 reducer 값을 넣는다
+//createSlice에 초기값과 reducer 값을 넣는다.
 export const counter = createSlice({
   name: "counter",
   // 초기값
   initialState: {
-    value:0
+    value: 4,
   },
-  // 실행할 ruducer => 객체형태로 익명함수를 할당해서 사용
-  reducers:{
-    increment : (state) => {
-      // toolkit을 이용하면 상태보존이 되기 때문에 
-      // 바로 값에 접근해서 수정가능
-      state.value += 1
+  // 실행할 reducer > 객체형태로 익명함수를 할당해서 사용.
+  reducers: {
+    increment: (state) => {
+      // 툴킷을 이용하면상태보존이 되기때문에
+      // 값에 바로 접근해서 수정가능
+      state.value += 1;
     },
-    incrementByAmount : 
-  }
+    incrementByAmount: (state, action) => {
+      // 외부에서 값을 들고 오고싶을때 action.payload을 통해 들고옴
+      state.value += action.payload;
+    },
+  },
+});
+
+// 액션타입을 함수로 만들어서 내보내기
+export const { increment, incrementByAmount } = counter.actions;
+
+// 디스패치를 따로 내보내줌
+export default counter.reducer;
